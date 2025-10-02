414b4cd7577ba576bfdce5bbf94a4f531721de8f
dancelhal2000 <aobinaobin15@gmail.com>
2025-10-02 10:47:10 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS16.java
+++ jobsheet5/ifCetakKRS16.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class ifCetakKRS16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String pesan;
 
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        pesan = (uktLunas) ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+                : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
 
         sc.close();
     }
